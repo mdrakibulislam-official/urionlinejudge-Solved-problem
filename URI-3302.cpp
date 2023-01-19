ef0602521d9a8bc4b0bb90aaa34571d8a700b8fa
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-19 18:29:29 +0600

    Create URI-3302.cpp

---
+++ URI-3302.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n,answer;
+
+int num=0;
+cin>>n;
+for(int i=0; i<n; i++){
+    cin>>answer;
+    cout<<"resposta "<<i+1<<": "<<answer<<endl;
+}
+}
