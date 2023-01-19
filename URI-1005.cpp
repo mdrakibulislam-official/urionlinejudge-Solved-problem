158a5ab90764d9752a7c607258a03aaba3fdb463
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-19 11:06:18 +0600

    Create URI-1005.cpp

---
+++ URI-1005.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    float a,b,c;
+    scanf("%f %f",&a,&b);
+    c=(3.5*a+7.5*b)/11;
+    printf("MEDIA = %.5f\n",c);
+    return 0;
+}
