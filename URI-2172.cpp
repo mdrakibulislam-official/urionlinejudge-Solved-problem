004fcb6e62251f0d6408edf598816d4ef86717c7
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 23:20:59 +0600

    Create URI-2172.cpp

---
+++ URI-2172.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int x;
+unsigned long long int m,z;
+while(1){
+    cin>>x>>m;
+    if(x==0 && m==0){
+        break;
+    }
+    z=x*m;
+    cout<<z<<endl;
+}
+}
