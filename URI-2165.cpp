ecc56f135c966452bd61ec9d68cda8aca6486dbc
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 23:23:15 +0600

    Create URI-2165.cpp

---
+++ URI-2165.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+    if(s.length()<=140){
+        cout<<"TWEET"<<endl;
+    }
+    else{
+        cout<<"MUTE"<<endl;
+    }
+
+
+}
