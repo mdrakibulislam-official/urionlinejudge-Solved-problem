7b22dc29583b09b6fe1324526ffc499364b2d42a
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 21:32:53 +0600

    Create URI-2787.cpp

---
+++ URI-2787.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int x,y;
+cin>>x>>y;
+if((x%2==0 && y%2==0) || (x%2!=0 && y%2!=0)){
+    cout<<1<<endl;
+}
+else{
+    cout<<0<<endl;
+}
+
+}
