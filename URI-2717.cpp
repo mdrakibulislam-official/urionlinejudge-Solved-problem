71cbb92d4e797f4ece5abaae989beef3d3377cb6
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 18:22:17 +0600

    Create URI-2717.cpp

---
+++ URI-2717.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int N, man1, man2;
+cin>>N;
+cin>>man1>>man2;
+if(man1+man2 > N){
+    cout<<"Deixa para amanha!"<<endl;
+}
+else{
+    cout<<"Farei hoje!"<<endl;
+}
+
+}
