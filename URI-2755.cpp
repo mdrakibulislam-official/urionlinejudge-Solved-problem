f2b53c4ade0c9be3c225f20d3d95669ccd4e595f
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-21 00:17:04 +0600

    Create URI-2755.cpp

---
+++ URI-2755.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<'"';
+cout<<"Ro'b'er"<<'\t'<<"to";
+cout<<"\\/"<<'"'<<endl;
+cout<<"(._.) ( l: ) ( .-. ) ( :l ) (._.)"<<endl;
+cout<<"(^_-) (-_-) (-_^)"<<endl;
+cout<<'('<<'"'<<'_'<<'"'<<") ('.')"<<endl;
+}
