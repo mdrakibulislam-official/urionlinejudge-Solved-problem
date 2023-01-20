82ee24c9f7a0bfba0f016dc366837b27aa56503c
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-21 00:27:11 +0600

    Create URI-1174.c

---
+++ URI-1174.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int i,b,e,z,k;
+	double a[100];
+	for(i=0;i<100;i++){
+		scanf("%lf",&a[i]);
+			
+		}
+	for(i=0;i<100;i++){
+		if(a[i]<=10){
+		printf("A[%d] = %.1lf\n",i,a[i]);
+		}
+		}}
