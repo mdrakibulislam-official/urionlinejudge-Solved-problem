4e5d31da55e4f27fd165ab771f09ddfc42ab3f5e
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 21:52:54 +0600

    Create URI-2147.cpp

---
+++ URI-2147.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    double k;
+int n;
+cin>>n;
+while(n--){
+cin>>s;
+k=s.length()*.01;
+cout<<fixed<<setprecision(2)<<k<<endl;
+}
+}
