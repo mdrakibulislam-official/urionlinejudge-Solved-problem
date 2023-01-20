c8c8be9bee2543e2f36df14bcc36d922718dcd9d
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-21 00:11:45 +0600

    Create URI-1181.cpp

---
+++ URI-1181.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,i,j;
+double box[12][12];
+char p;
+double sum=0.0;
+cin>>t>>p;
+for( i=0;i<12;i++){
+    for(j=0;j<12;j++){
+        cin>>box[i][j];
+        if(t==i){
+            sum+=box[i][j];
+        }
+    }
+}
+if(p=='S'){
+    cout<<fixed<<setprecision(1)<<sum<<endl;
+}
+else if(p=='M'){
+    cout<<fixed<<setprecision(1)<<sum/12.0<<endl;
+}
+}
