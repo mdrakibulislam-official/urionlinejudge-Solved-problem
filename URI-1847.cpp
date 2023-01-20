82b0d0d4d65dd5f658d3f7a9c2270f5758d34958
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 21:16:18 +0600

    Create URI-1847.cpp

---
+++ URI-1847.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int x,y,z;
+	scanf("%d%d%d",&x,&y,&z);
+
+	if(x>y && z>=y)
+	cout << ":)" << endl;
+	else if (y>x && z<=y)
+	cout << ":(" << endl;
+    else if (y>x && z>y && z-y < y-x)
+	cout << ":(" << endl;
+	else if (y>x && z>y && z-y >= y-x)
+	cout << ":)" << endl;
+	else if (x>y && y>z && z-y > y-x )
+    cout << ":)" << endl;
+    else if (x>y && y>z && z-y <= y-x)
+    cout << ":(" << endl;
+    else if (x==y && z>y)
+    cout << ":)" << endl;
+    else if (x==y && z<y)
+    cout << ":(" << endl;
+    else cout << ":(" << endl;
+}
