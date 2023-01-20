5859a4fafde6eeec0c792717354a5360902ef650
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 23:25:46 +0600

    Create URI-2582.cpp

---
+++ URI-2582.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n,a,b,i,k;
+cin>>n;
+for(i=0;i<n;i++){
+    cin>>a>>b;
+    if(a+b==0){
+        cout<<"PROXYCITY"<<endl;
+    }
+    else if(a+b==1){
+        cout<<"P.Y.N.G."<<endl;
+    }
+    else if(a+b==2){
+        cout<<"DNSUEY!"<<endl;
+    }
+    else if(a+b==3){
+        cout<<"SERVERS"<<endl;
+    }
+    else if(a+b==4){
+        cout<<"HOST!"<<endl;
+    }
+    else if(a+b==5){
+        cout<<"CRIPTONIZE"<<endl;
+    }
+    else if(a+b==6){
+        cout<<"OFFLINE DAY"<<endl;
+    }
+    else if(a+b==7){
+        cout<<"SALT"<<endl;
+    }
+    else if(a+b==8){
+        cout<<"ANSWER!"<<endl;
+    }
+    else if(a+b==9){
+        cout<<"RAR?"<<endl;
+    }
+    else if(a+b==10){
+        cout<<"WIFI ANTENNAS"<<endl;
+    }
+}
+
+}
