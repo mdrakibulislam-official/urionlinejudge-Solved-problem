6c4d1a1b1fd76193df67c8e65a506091d7b7c133
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-20 21:25:48 +0600

    Create URI-2483.cpp

---
+++ URI-2483.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a;
+cin>>a;
+    cout<<"Feliz nat";
+    for(int i=1; i<=a;i++){
+        cout<<'a';
+    }
+    cout<<"l!"<<endl;
+}
