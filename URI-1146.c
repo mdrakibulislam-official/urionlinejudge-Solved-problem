51f3c422be98f36c944aedef5b9e3b392647b4cd
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-21 00:39:32 +0600

    Create URI-1146.c

---
+++ URI-1146.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+		int a,k=1,i;
+		while(scanf("%d",&a)&&a){
+			//if(a==1)
+	        //printf("%d\n",a);
+	        if(a>=1)
+	        printf("%d",k);
+	         for(i=2;i<=a;i++){
+		printf(" %d",i);
+			}printf("\n");
+	}}
