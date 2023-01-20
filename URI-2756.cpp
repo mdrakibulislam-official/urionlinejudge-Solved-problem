082e4d24cc0401d59fbac053d25a592b050e4a77
Md. Rakibul Islam <99571909+mdrakibulislam-official@users.noreply.github.com>
2023-01-21 00:17:43 +0600

    Create URI-2756.cpp

---
+++ URI-2756.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i,j,k;
+cout<<"       A"<<endl;
+cout<<"      B B"<<endl;
+cout<<"     C   C"<<endl;
+cout<<"    D     D"<<endl;
+cout<<"   E       E"<<endl;
+cout<<"    D     D"<<endl;
+cout<<"     C   C"<<endl;
+cout<<"      B B"<<endl;
+cout<<"       A"<<endl;
+}
